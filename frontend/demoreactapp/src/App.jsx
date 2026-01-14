c57a33a378670e550cff5bec3ee3d66447436adb
PARDHASARADHI REDDY <2300032590@kluniversity.in>
2026-01-14 22:09:01 +0530

    Add files via upload

---
+++ frontend/demoreactapp/src/App.jsx
@@ -0,0 +1,14 @@
+import './style.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React Project - S205</h1>
+      <p>CI/CD and Cloud DevOps</p>
+      <p>This is a simple homepage deployed on GitHub <span>Actions</span> using Vite.</p>
+      <p>Done with Git pages & Git Actions</p>
+    </div>
+  );
+}
+
+export default App;
